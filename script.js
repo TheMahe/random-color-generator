452fa5b46c4f794d93308d6320349bf942c83262
TheMahe <mahir.delic.07@gmail.com>
2024-01-15 12:38:22 +0100

    added more functionalities

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-document.getElementById('colorBtn').addEventListener('click', function() {
-    var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-    document.getElementById('colorDisplay').style.backgroundColor = randomColor;
-    document.getElementById('colorDisplay').innerText = randomColor;
+function generateRandomColor() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function lightenDarkenColor(col, amt) {
+    var usePound = false;
+    if (col[0] == "#") {
+        col = col.slice(1);
+        usePound = true;
+    }
+    var num = parseInt(col,16);
+    var r = (num >> 16) + amt;
+    if (r > 255) r = 255;
+    else if (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+    if (b > 255) b = 255;
+    else if (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
+document.getElementById('solidColorBtn').addEventListener('click', function() {
+    var color = generateRandomColor();
+    document.getElementById('colorDisplay').style.backgroundColor = color;
+    document.getElementById('colorDisplay').innerText = color;
 });
+
+document.getElementById('lighterColorBtn').addEventListener('click',function() {
+    var baseColor = generateRandomColor();
+    var lighterColor = lightenDarkenColor(baseColor, 40);
+    document.getElementById('colorDisplay').style.backgroundColor = lighterColor;
+    document.getElementById('colorDisplay').innerText = lighterColor;
+    });
+    
+    document.getElementById('darkerColorBtn').addEventListener('click', function() {
+    var baseColor = generateRandomColor();
+    var darkerColor = lightenDarkenColor(baseColor, -40);
+    document.getElementById('colorDisplay').style.backgroundColor = darkerColor;
+    document.getElementById('colorDisplay').innerText = darkerColor;
+    });
